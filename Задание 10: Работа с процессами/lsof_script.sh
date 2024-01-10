27255151fb28234f29a3a1684b7dbf72645b1eb0
eozhmegov <eozhmegov@ozon.ru>
2024-01-10 16:26:01 +0300

    task 10 so added

--- Задание 10: Работа с процессами/lsof_script.sh
+++ Задание 10: Работа с процессами/lsof_script.sh
@@ -10,9 +10,14 @@ echo 'NAME PROC_ID FD'
 for PROC in $PROCESSES
 do
   FD=$(sudo ls -l /proc/$PROC/fd/ 2>/dev/null |  tail -n +2 |  awk '{print $NF}')
+  SO=$(sudo ls -l /proc/$PROC/map_files 2>/dev/null |  tail -n +2 |  awk '{print $NF}' | uniq)
   NAME=$(sudo ls -l /proc/$PROC/exe 2>/dev/null | awk '{print $NF}')
   for DIR in $FD
     do
       echo "$NAME $PROC $(echo $DIR)"
     done
+  for ENTRY in $SO
+  do
+    echo echo "$NAME $PROC $(echo $ENTRY)"
+  done
 done
