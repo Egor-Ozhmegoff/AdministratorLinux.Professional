e71854f666ac864ba2bb905cffa0383b645d4cb6
eozhmegov <eozhmegov@ozon.ru>
2024-01-15 14:16:11 +0300

    Task 9 added

--- Task-9/mail_script.sh
+++ Task-9/mail_script.sh
@@ -2,12 +2,14 @@
 EMAIL_ADDRESS=$1
 PROCESS_COUNT=$(pidof -x "mail_script.sh" | awk '{ print NF }')
 
+# Проверяем запущен такой скрипт или нет, если запущен то оастанавливаем скрипт
 if  (( "$PROCESS_COUNT" > 2 ))
 then
     echo "Process already running"
     exit 1
 fi
 
+# Функция которая формирует лог
 function get_log {
     echo "Log from $(date -d "-1 hour") to $(date)" > mail_script.log
     awk '{ print $1 }' /var/log/nginx/access.log | uniq -c | awk '{ print "IP " $2 " REQUESTS " $1}' >> mail_script.log
@@ -18,8 +20,9 @@ function get_log {
     truncate -s 0 /var/log/nginx/error.log
 }
 
+# Функция отправки почты
 function send_mail {
-    cat /tmp/mail_script | mail -s "log" $EMAIL_ADDRESS
+    cat mail_script.log | mail -s "log" $EMAIL_ADDRESS
 }
 
 get_log
