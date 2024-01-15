e4f741f683c414b183fd3c3999f933f3924dbf8e
eozhmegov <eozhmegov@ozon.ru>
2024-01-15 13:20:21 +0300

    Task 9 added

--- Task-9/mail_script.sh
+++ Task-9/mail_script.sh
@@ -9,14 +9,13 @@ then
 fi
 
 function get_log {
-    echo '' > mail_script.log
-    echo "Log from $(date -d "-1 hour") to $(date)"
+    echo "Log from $(date -d "-1 hour") to $(date)" > mail_script.log
     awk '{ print $1 }' /var/log/nginx/access.log | uniq -c | awk '{ print "IP " $2 " REQUESTS " $1}' >> mail_script.log
     grep "http" /var/log/nginx/access.log | awk '{ print $ 11 }' | uniq -c | awk '{ print "URL " $2 " REQUESTS " $1}' >> mail_script.log
     cat /var/log/nginx/error.log >> mail_script.log
     awk '{ print $9 }' /var/log/nginx/access.log | uniq -c | awk '{ print "HTTP CODE " $2 " COUNT " $1}' >> mail_script.log
-    echo '' > /var/log/nginx/access.log
-    echo '' > /var/log/nginx/error.log
+    truncate -s 0 /var/log/nginx/access.log
+    truncate -s 0 /var/log/nginx/error.log
 }
 
 function send_mail {
